5e60fa95479f307a394fcf2d6a0a5e3c24c3694f
Rommel Ulco <rulco@unitru.edu.pe>
2025-03-07 19:52:22 -0500

    Deplo0

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,8 @@ return [
     'allowed_origins' => [
         'http://localhost:3000',
         'http://26.155.91.192:3000',
-        'https://votaciones-ivory.vercel',
+        'https://votaciones-ivory.vercel.app',
+        'https://votaciones-git-master-urommels-projects.vercel.app',
         'https://votaciones-708621156652.us-central1.run.app',
     ],
     'allowed_origins_patterns' => [],
