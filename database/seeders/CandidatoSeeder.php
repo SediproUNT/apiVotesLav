3974a2006f7f2857648030b60de8d9c93c79de26
Rommel Ulco <rulco@unitru.edu.pe>
2025-03-25 11:01:24 -0500

    Arreglo del candidato

--- database/seeders/CandidatoSeeder.php
+++ database/seeders/CandidatoSeeder.php
@@ -19,7 +19,7 @@ class CandidatoSeeder extends Seeder
 
             //Presidenta
             [
-                'sediprano_id' => 78, //Lucía de Fátima Amaya Cáceda
+                'sediprano_id' => 75, //Lucía de Fátima Amaya Cáceda
                 'cargo_id' => 1,  //Presidenta
                 'area_id' => NULL,
                 'votacion_id' => 1,
@@ -29,14 +29,14 @@ class CandidatoSeeder extends Seeder
 
             //MKT
             [
-                'sediprano_id' => 46, //Angel Iparraguirre Aguilar
+                'sediprano_id' => 44, //Angel Iparraguirre Aguilar
                 'cargo_id' => 2,  //Director
                 'area_id' => 3,  //Marketing
                 'votacion_id' => 1,
                 'foto' => 'https://storage.googleapis.com/imagenes_bananos/votes/2025/ANGEL%20IPARRAGUIRRE%20AGUILAR.png',
             ],
             [
-                'sediprano_id' => 50, //Cielo Valentina Abanto Rojas
+                'sediprano_id' => 48, //Cielo Valentina Abanto Rojas
                 'cargo_id' => 2,  //Director
                 'area_id' => 3,  //Marketing
                 'votacion_id' => 1,
@@ -45,14 +45,14 @@ class CandidatoSeeder extends Seeder
 
             // LKT
             [
-                'sediprano_id' => 26, //Diego Jesus Rodriguez Sabana
+                'sediprano_id' => 24, //Diego Jesus Rodriguez Sabana
                 'cargo_id' => 2,  //Director
                 'area_id' => 2,  //Logística y Finanzas
                 'votacion_id' => 1,
                 'foto' => 'https://storage.googleapis.com/imagenes_bananos/votes/2025/DIEGO%20JESUS%20-%20DIEGO%20JESUS%20RODRIGUEZ%20SABANA.jpeg',
             ],
             [
-                'sediprano_id' => 36, //Kevin Gamaliel Rodríguez Alfaro
+                'sediprano_id' => 34, //Kevin Gamaliel Rodríguez Alfaro
                 'cargo_id' => 2,  //Director
                 'area_id' => 2,  //Logística y Finanzas
                 'votacion_id' => 1,
@@ -61,7 +61,7 @@ class CandidatoSeeder extends Seeder
 
             //GTH
             [
-                'sediprano_id' => 14, //Marina Lizeth Gonzales Torres
+                'sediprano_id' => 13, //Marina Lizeth Gonzales Torres
                 'cargo_id' => 2,  //Director
                 'area_id' => 5,  //Gestión de Talento Humano
                 'votacion_id' => 1,
@@ -70,7 +70,7 @@ class CandidatoSeeder extends Seeder
 
             //PMO
             [
-                'sediprano_id' => 80, //María Fernanda De La Caridad Herrera Cerquin
+                'sediprano_id' => 77, //María Fernanda De La Caridad Herrera Cerquin
                 'cargo_id' => 2,  //Director
                 'area_id' => 1,  //PMO
                 'votacion_id' => 1,
@@ -79,11 +79,11 @@ class CandidatoSeeder extends Seeder
             
             //TI
             [
-                'sediprano_id' => 89, //Christian Anthony Morales Esquivel
+                'sediprano_id' => 85, //Christian Anthony Morales Esquivel
                 'cargo_id' => 2,  //Director
                 'area_id' => 4,  //Tecnologías de la Información
                 'votacion_id' => 1,
-                'foto' => ' ',
+                'foto' => 'https://storage.googleapis.com/imagenes_bananos/votes/2025/yiooo.png',
             ],
 
 
